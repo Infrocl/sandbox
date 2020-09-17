c80139fed5459133209e59defe6cf6fc5c1ac6f1
Kolebanova <infrocl@yandex.ru>
2020-09-17 23:45:37 +0400

    Обновлены классы SqrtOperation, TangentOperation, создано несколько тестов для методов класса TangentOperation.

--- sandbox/src/main/java/ru/ssau/tk/sandman/sandbox/simpleclasses/TangentOperation.java
+++ sandbox/src/main/java/ru/ssau/tk/sandman/sandbox/simpleclasses/TangentOperation.java
@@ -1,7 +1,21 @@
 package ru.ssau.tk.sandman.sandbox.simpleclasses;
 
+
 public class TangentOperation extends Operation {
-    public double apply(double number) {
+    @Override
+    public double apply(double number) { //радианы
+        if (Double.isNaN(number)) {
+            throw new ArithmeticException("Can't calculate the tangent of the value: it's NaN.");
+        }
+        if (Double.isInfinite(number)) {
+            throw new ArithmeticException("Can't calculate the tangent of the value: it's infinite.");
+        }
+        /*
+        if ((number!=0) && (Math.abs(number)!=Math.PI) && (Math.abs(number % (Math.PI/2)) ) <= 0.000005) {
+            throw new ArithmeticException("Tangent of the " + number + " doesn't exist. It's limit becomes infinite.");
+            //TODO: переделать условие; не работает для чисел, близких к нулю
+        }
+         */
         return Math.tan(number);
     }
 }
--- sandbox/src/test/java/ru/ssau/tk/sandman/sandbox/simpleclasses/TangentOperationTest.java
+++ sandbox/src/test/java/ru/ssau/tk/sandman/sandbox/simpleclasses/TangentOperationTest.java
@@ -1,4 +1,30 @@
 package ru.ssau.tk.sandman.sandbox.simpleclasses;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class TangentOperationTest {
+    public static double ACCURACY = 0.00005;
+
+    @Test
+    public void testApplyNumber() {
+        TangentOperation tangent = new TangentOperation();
+        assertEquals(tangent.apply(Math.PI / 3), 1.73205, ACCURACY);
+        assertEquals(tangent.apply(0), 0, ACCURACY);
+        assertEquals(tangent.apply(-Math.PI), 0, ACCURACY);
+    }
+
+    @Test
+    public void testApplyInfinity() {
+        TangentOperation tangent = new TangentOperation();
+        assertThrows(ArithmeticException.class, () -> tangent.apply(Double.POSITIVE_INFINITY));
+        assertThrows(ArithmeticException.class, () -> tangent.apply(Double.NEGATIVE_INFINITY));
+    }
+
+    @Test
+    public void testApplyNan() {
+        TangentOperation tangent = new TangentOperation();
+        assertThrows(ArithmeticException.class, () -> tangent.apply(Double.NaN));
+    }
 }
